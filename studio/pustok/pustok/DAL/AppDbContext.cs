6d595997134997b7629ab0e4da59b66fdc62e6ee
unknown <Administrator@DESKTOP-GE46997.aztu.local>
2022-05-23 04:31:32 -0700

    first commit

---
+++ studio/pustok/pustok/DAL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pustok.DAL
+{
+    public class AppDbContext : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
